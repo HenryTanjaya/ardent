8b86b03d2de57428b036579fdb61d8e1abae1536
HenryTanjaya <henrytanjaya11@gmail.com>
2017-11-29 13:45:27 +0700

    add changeable featured event landing

--- models/landing.js
+++ models/landing.js
@@ -6,5 +6,17 @@ var landingSchema = new mongoose.Schema({
    imagemission:String,
    imagevalues:String,
    imagecontact:String,
+   featuredname1:String,
+   featuredname2:String,
+   featuredname3:String,
+   featuredimage1:String,
+   featuredimage2:String,
+   featuredimage3:String,
+   featureddate1:String,
+   featureddate2:String,
+   featureddate3:String,
+   featuredlink1:String,
+   featuredlink2:String,
+   featuredlink3:String
 });
 module.exports = mongoose.model("Landing", landingSchema);
